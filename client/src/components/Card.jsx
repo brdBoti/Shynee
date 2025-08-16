6c9bf713eca81b3a384597f6f77fca5153d58b7c
Burda Boti <burdabotond@gmail.com>
2025-08-16 20:17:38 +0200

    card flip

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,44 +1,27 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Card.css';
 
 export default function Card({ color = "silver", title, price, subtitle, features = [], buttonText, page }) {
-
-  let linkTo = 'https://spotless.salonic.hu/';
-
-  if (page === 'rendelesbelso') {
-    if (title === 'Alap') linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404515';
-    else if (title === 'Prémium') {
-      linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404516';
-    }
-  } else if (page === 'rendeleskulso') {
-    if (title === 'Alap') linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404339';
-    else if (title === 'Prémium') {
-      linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404340';
-    }
-  } else if (page === 'rendeleskulsobelso') {
-    if (title === 'Alap') linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404517';
-    else if (title === 'Prémium') linkTo = 'https://shynee.salonic.hu/selectEmployee/?placeId=12457&serviceId=404518';
-  }
-
+  const [flipped, setFlipped] = useState(false);
   return (
-    <div className={`card card-${color}`}>
-      <span className="title">{title}</span>
-      <p className="pricing">
-        {price} <span className="pricing-time">/ alkalom</span>
-      </p>
-      <span className="sub-title">{subtitle}</span>
-      <ul className="list">
-        {features.map((feature, i) => (
-          <li className="list-item" key={i}>
-            <span className="check">✓</span> {feature}
-          </li>
-        ))}
-      </ul>
-      <Link to={linkTo}>
-        <button className="button">
-          <span className="text-button">{buttonText}</span>
-        </button>
-      </Link>
+    <div className="flip-card-container">
+      <div className={`flip-card${flipped ? ' flipped' : ''}`}>
+        <div className="flip-card-front">
+          <span className="title">{title}</span>
+          <p className="pricing">{price}</p>
+          <button className="flip-btn" onClick={() => setFlipped(true)}>
+            Show Back
+          </button>
+        </div>
+        <div className="flip-card-back">
+          <h1>Back of Card</h1>
+          <p>More info</p>
+          <button className="flip-btn" onClick={() => setFlipped(false)}>
+            Show Front
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
